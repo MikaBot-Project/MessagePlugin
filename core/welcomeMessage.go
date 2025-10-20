9fda8529c4786760de23b002ca4bfa72b445c149
yuanxy-acy <yuanxy.acy@outlook.com>
2025-10-20 18:26:03 +0800

    update welcome message

--- core/welcomeMessage.go
+++ core/welcomeMessage.go
@@ -9,6 +9,7 @@ import (
 
 func WelcomeMessage(msg pluginIO.Message) {
 	message, ok := config.WelcomeMsgMap[strconv.FormatInt(msg.GroupId, 10)]
+	msg.AtMe = true
 	if ok {
 		sendMessage(message, &msg)
 	} else {
